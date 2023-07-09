dd2b5d944ebde3aead71f7afd8c52e25de3cf9e4
h5tusyakdiah <121065596+h5tusyakdiah@users.noreply.github.com>
2023-07-09 15:20:52 +0800

    Update P&L.py

--- P&L.py
+++ P&L.py
@@ -4,8 +4,7 @@ import numpy as np
 from pathlib import Path
 from dateutil.relativedelta import relativedelta
 import datetime as dt
-import math
-import os
+from openpyxl import Workbook
 
 st.set_page_config(page_title="VAMOS", 
                    page_icon=":bar_chart:", 
@@ -18,140 +17,149 @@ st.text("")
 
 
 #parameter1
+col0, col00, col000, col0000, col00000 = st.columns([1, 1, 1, 1, 1])
+with col0:
+    nama_kapal_input = st.selectbox("Ship Class",["KM Pusri Indonesia 1", "KM Julianto Moeliodihardjo"])
+with col00:
+    loading_port = st.selectbox("Loading Port",["Banyuwangi", "Belawan", "Bengkulu","Bontang","Cigading",
+                                                   "Cilacap","Dumai","Gresik","Lampung","Lembar", "Lhokseumawe",
+                                                   "Makassar","Padang","Palembang","Semarang","Sorong","Surabaya"])
+with col000:
+    discharge_port = st.selectbox("Discharge Port",["Banyuwangi", "Belawan", "Bengkulu","Bontang","Cigading",
+                                                   "Cilacap","Dumai","Gresik","Lampung","Lembar", "Lhokseumawe",
+                                                   "Makassar","Padang","Palembang","Semarang","Sorong","Surabaya"])
+with col0000:
+    voyage = st.selectbox("Voyage",[1, 2, 3, 4, 5, 6 , 7 , 8 ,  9 , 10])
+
 co1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1])
 with co1:
-    nama_kapal_input = st.selectbox("Nama Kapal",["KM PUSRI INDONESIA", "KM MOCHTAR PRABU MANGKUNEGARA", "KM ANUGERAH BUANA III"])
+    Freight_ton = st.number_input("Freight per ton", value = 415000.0, min_value=0.0, help="xxx", step=1e-6, format="%.1f")
 with col2:
-    LSFO_price = st.number_input("LSFO Price", help="Bahan bakar ketika berlayar", step=1e-6, format="%.1f")
+    COB = st.number_input("COB", help="Bahan bakar ketika berlayar", value=7000.0, step=1e-6, format="%.1f")
 with col3:
-    Fuel_Cons1 = st.number_input("Fuel Consumption",value =26,min_value=0, help="xxx")
+    Discharge_rate = st.number_input("Discharge rate",value =4000,min_value=0, help="xxx")
 with col4:
-    sailing_days = st.number_input("Sailing Days",min_value=0.0, help="xxx", step=1e-6, format="%.1f")
+    Loading_rate = st.number_input("Loading rate",value =2400,min_value=0, help="xxx")
 with col5:
-    HSD_Price = st.number_input("HSD Price",value =705,min_value=0, help="xxx")
+    Freetime_1 = st.number_input("Free Time at PoL", value=0,min_value=0, help="xxx")
 
 #parameter part 2
-co6, col7, col8, col9, col10 = st.columns([1, 1, 1, 1, 1])
-with co6:
-    Fuel_Cons2 = st.number_input("Fuel Consumption AE", value=1,min_value=0, help="xxx")
+col6, col7, col8, col9, col10 = st.columns([1, 1, 1, 1, 1])
+with col6:
+    Freetime_2 = st.number_input("Free Time at PoD", value=0,min_value=0, help="xxx")
 with col7:
-    Fixed_Cost = st.number_input("Fixed Cost",value =15125,min_value=0, help="xxx")
+    Fixed_Cost = st.number_input("Fixed Cost", value=30000000,min_value=0, help="xxx")
 with col8:
-    Cargo_Volume = st.number_input("Cargo Volume",value =15,min_value=0, help="xxx")
+    ME_Fuel_Cons = st.number_input("ME Fuel Cons",value =12,min_value=0, help="xxx")
 with col9:
-    Discharge_rate = st.number_input("Discharge rate",value =4000,min_value=0, help="xxx")
+    AE_Fuel_Cons = st.number_input("AE Fuel Cons",value =1.4,min_value=0.0, help="xxx", step=1e-6, format="%.1f")
 with col10:
-    Loading_rate = st.number_input("Loading rate",value =4000,min_value=0, help="xxx")
+    Port_Charges = st.number_input("Port Charges",value =60000000,min_value=0, help="xxx")   
 
 #parameter part 3
 
 co111, col12, col13, col14, col15 = st.columns([1, 1, 1, 1, 1])
 with co111:
-    Freight_ton = st.number_input("Freight per ton",value =20.0, min_value=0.0, help="xxx", step=1e-6, format="%.1f")
-    
+    LSFO_Price = st.number_input("LSFO Price", value=19700,min_value=0, help="LSFO or HSD")
+with col12:
+    HSD_Price = st.number_input("HSD Price", value=20700,min_value=0, help="xxx")
 
-#parameter part 4
 
-co116, col17, col18, col19, col20 = st.columns([1, 1, 1, 1, 1])
-with co116:
-    cost_realization = st.number_input("Cost Realization",min_value=0.0, help="xxx", step=1e-6, format="%.1f")
-with col17:
-    budget = st.number_input("Budget",value=1.0, min_value=0.0, help="xxx", step=1e-6, format="%.1f")
-with col18:
-    variable_cost = st.number_input("Variable Cost",value=1.0, min_value=0.0, help="xxx", step=1e-6, format="%.1f")
+#co116, col17, col18, col19, col20 = st.columns([1, 1, 1, 1, 1])
+#with co116:
+#    cost_realization = st.number_input("Cost Realization",min_value=0.0, help="xxx", step=1e-6, format="%.1f")
+#with col17:
+#    budget = st.number_input("Budget",value=1.0, min_value=0.0, help="xxx", step=1e-6, format="%.1f")
+#with col18:
+#    variable_cost = st.number_input("Variable Cost",value=1.0, min_value=0.0, help="xxx", step=1e-6, format="%.1f")
 
 #RUN CALCULATION
 
 hitung = st.button("Hitung") 
 
 if hitung:
-    data1 = pd.read_csv("dummy simulasi.csv", sep=",")
-    data1['Time_arrived'] = pd.to_datetime(data1['Time_arrived'])
-    data1['Time_depature'] = pd.to_datetime(data1['Time_depature'])
-    data1["operate_day"] = data1["Time_arrived"] - data1["Time_depature"]
-    data1["operate_day"] = data1["operate_day"].astype(str)
-    data1["operate_day"] = data1["operate_day"].str[:2]
-
-    #menghitung realisasi
-    data_filter = data1[(data1.Nama_Kapal == nama_kapal_input)]
-    data_filter["operate_day"] = data_filter["operate_day"].astype(int)
-    realisasi = data_filter["Time_elapsed"]/data_filter["Time_planned"]
-    realisasi = realisasi.sum()
-
-    #menghitung speed
-    speed = (data_filter["operate_day"]/data_filter["Standard_time_planned"])*100
-    speed = speed.sum()
-
-    #menghitung P & L
-    Distance = 1207
-    Speed = 14
-    sailing_days =  Distance*2/(Speed*24) 
-    LSFO_price =  LSFO_price*1.1
-    Bunker_Sailing= sailing_days*LSFO_price*Fuel_Cons1*-1
-    HSD_Price = HSD_Price*1.1
-    Port_Days = (Cargo_Volume/Loading_rate)+(Cargo_Volume/Discharge_rate)
-    Bunker_at_Port = HSD_Price*Port_Days*Fuel_Cons2*-1
-    Total_Days = Port_Days+sailing_days
-    Port_Charges = 2.05*Cargo_Volume*-1
-    Total_Fixed_cost = Total_Days*Fixed_Cost*-1 
-    Total_cost = Port_Charges+Total_Fixed_cost+Bunker_at_Port+Bunker_Sailing
-    Revenue = Cargo_Volume*Freight_ton
-    P_and_L = Total_cost+Revenue
-
-    #menghitung Cost Accuracy
-    Cost_Accuracy = (cost_realization/budget)*100
-
-    #menghitung BEP
-    BEP = P_and_L/(Fixed_Cost-variable_cost)
     
-    st.markdown(f"Kapal **{(nama_kapal_input)}** memiliki nilai")
+    #input data
+    Vessel_List = pd.read_excel('Rekap Vessel Performance.xlsx', sheet_name='Vessel List')
+    OD_Matrix = pd.read_excel('Rekap Vessel Performance.xlsx', sheet_name='OD Matrix')
+    Time_Sheet = pd.read_excel('Rekap Vessel Performance.xlsx', sheet_name='Input - Time Sheet')
 
+    #calculating
+#ACTUAL
+    PoL_PoD = OD_Matrix[(OD_Matrix.port1 == loading_port) & (OD_Matrix.port2 == discharge_port)]
+    Distance = PoL_PoD.distance.sum()
+
+    Service_speed = 6.7
+    Sailing_Days = Time_Sheet[(Time_Sheet.Vessel_Name == nama_kapal_input) & (Time_Sheet.PoL == loading_port) 
+                            & (Time_Sheet.PoD == discharge_port) & (Time_Sheet.Voyage == voyage)]
+    Sailing_Days = Sailing_Days[(Sailing_Days.Activity == "Sailing to PoD")]
+    Sailing_Days = Sailing_Days.Dur.sum()
+
+    Port_Days= Time_Sheet[(Time_Sheet.Vessel_Name == nama_kapal_input) & (Time_Sheet.PoL == loading_port) 
+                            & (Time_Sheet.PoD == discharge_port) & (Time_Sheet.Voyage == voyage)]
+    Port_Days = Port_Days[(Port_Days.Activity == "Loading") | (Port_Days.Activity == "Discharge")]
+    Port_Days = Port_Days.Dur.sum()
+
+    Waiting_Days = Time_Sheet[(Time_Sheet.Vessel_Name == nama_kapal_input) & (Time_Sheet.PoL == loading_port) 
+                            & (Time_Sheet.PoD == discharge_port) & (Time_Sheet.Voyage == voyage)]
+    Waiting_Days = Waiting_Days[(Waiting_Days.Activity == "Waiting Loading") | (Waiting_Days.Activity == "Waiting Discharge")]
+    Waiting_Days = Waiting_Days.Dur.sum()
+
+    Total_Days = Time_Sheet[(Time_Sheet.Vessel_Name == nama_kapal_input) & (Time_Sheet.PoL == loading_port) 
+                            & (Time_Sheet.PoD == discharge_port) & (Time_Sheet.Voyage == voyage)]
+    Total_Days = Total_Days.Dur.sum()
+    #Sailing_Days, Port_Days, Waiting_Days, Total_Days
+    Bunker_Sailing = (Sailing_Days*LSFO_Price*AE_Fuel_Cons*1000)+(Sailing_Days*LSFO_Price*ME_Fuel_Cons*1000)*-1
+    Bunker_at_Port = (Port_Days*HSD_Price*AE_Fuel_Cons*1000*2*-1)
+    Bunker_Waiting = (Waiting_Days*HSD_Price*AE_Fuel_Cons*1000*-1)
+    #Bunker_Sailing, Bunker_at_Port, Bunker_Waiting
+
+    Total_Fixed_cost = Total_Days*Fixed_Cost*-1
+    Total_cost = Bunker_Sailing + Bunker_at_Port + Bunker_Waiting + Port_Charges + Total_Fixed_cost
+    Revenue = Freight_ton*COB
+    P_and_L = Revenue + Total_cost
+
+    #-------------------------------------------------------------------
+    #IDEAL
+    Distance_2 = Distance 
+    filter_ideal =  Vessel_List[(Vessel_List.Nama_Kapal == nama_kapal_input)]
+    Service_speed_2 = filter_ideal.Speed_Max.sum()
+    Sailing_Days_2 =(Distance_2*2)/(Service_speed_2*24)
+    Port_Days_2 = (COB/Loading_rate)+(COB/Discharge_rate)
+    Waiting_Days_2 = 0
+    Total_Days_2 = Sailing_Days_2+Port_Days_2 + Waiting_Days_2
+    ME_Fuel_Cons_2 = filter_ideal.ME_Cons.sum()
+    AE_Fuel_Cons_2 = filter_ideal.AE_Cons.sum()
+    Bunker_Sailing_2 = ((Sailing_Days_2*LSFO_Price*ME_Fuel_Cons_2*1000)+(Sailing_Days_2*LSFO_Price*AE_Fuel_Cons_2*1000))*-1
+    Bunker_at_Port_2 = (Port_Days_2*HSD_Price*AE_Fuel_Cons_2*1000*2*-1)
+    Bunker_Waiting_2 = (Waiting_Days_2*HSD_Price*AE_Fuel_Cons_2*1000*-1)
+
+    Port_Charges_2 = 8000*COB*-1
+    Fixed_Cost_2 = 815000000/30
+    Total_Fixed_cost_2 = Total_Days_2*Fixed_Cost_2*-1
+    Total_cost_2 = Bunker_Sailing_2 + Bunker_at_Port_2 + Bunker_Waiting_2 + Port_Charges_2 + Total_Fixed_cost_2
+    Revenue_2 = Freight_ton*COB
+    P_and_L_2 = Revenue_2 + Total_cost_2
+#----------------------------------------------------------------------------
+    Remaining_Time	= Total_Days_2 - Total_Days
+    Time_accuracy =  Total_Days_2/Total_Days*100
+    Speed = Sailing_Days_2/Sailing_Days *100
+    P_and_L_actual	= P_and_L
+    Cost_accuracy  = Total_cost/Total_cost_2 *100
+
+    #Output
+    st.markdown(f"Kapal **{(nama_kapal_input)}** memiliki nilai")
+    #st.markdown(f"nilai **{(Bunker_Sailing)}**, **{(Bunker_at_Port)}**, **{(Bunker_Waiting)}**")
 
     co1A, colB, colC, colD, colE = st.columns([1, 1, 1, 1, 1])
-    co1A.metric(label = "Realisasi",
-                value = (f"{round(realisasi,2)} %"))
-    colB.metric(label = "Speed",
-                value = (f"{round(speed,2)} %"))
-    colC.metric(label = "P & L",
-                value = (f"Rp. {round(P_and_L)}"))
-    colD.metric(label = "Cost_Accuracy",
-                value = (f"{round(Cost_Accuracy,2)} %"))
-    colE.metric(label = "BEP",
-                value = (f"{round(BEP,2)} %"))
+    co1A.metric(label = "Remaining Time",
+                value = (f"{round(Remaining_Time,2)}"))
+    colB.metric(label = "Time accuracy",
+                value = (f"{round(Time_accuracy,2)} %"))
+    colC.metric(label = "Speed",
+                value = (f"{round(Speed)} %"))
+    colD.metric(label = "P & L",
+                value = (f"Rp. {round(P_and_L_actual)}"))
+    colE.metric(label = "Cost Accuracy",
+                value = (f"{round(Cost_accuracy,2)} %"))
     
-    #st.markdown(f"realisasi = **{round(realisasi,2)}** %")
-    #st.markdown(f"speed = **{round(speed,2)}** %")
-    #st.markdown(f"P & L = Rp. **{round(P_and_L)}**")
-    #st.markdown(f"Cost_Accuracy = **{round(Cost_Accuracy,2)}** %")
-    #st.markdown(f"BEP = **{round(BEP)}** %")
-    #st.markdown(f"Bunker_Sailing = **{round(Bunker_Sailing)}**, Port_Days = **{round(Port_Days)}** , Total_cost = **{round(Total_cost)}**  %")
-
-#-----------------
-# Disable certificate verification (Not necessary always)
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-
-from gspread_pandas import Spread,Client
-from google.oauth2 import service_account
-# Application Related Module
-import pubchempy as pcp
-from pysmiles import read_smiles
-# 
-import networkx as nx
-
-
-# Create a Google Authentication connection object
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-
-credentials = service_account.Credentials.from_service_account_info(
-                st.secrets["gcp_service_account"], scopes = scope)
-client = Client(scope=scope,creds=credentials)
-spreadsheetname = "Rekap Vessel Performance"
-spread = Spread(spreadsheetname,client = client)
-
-# Check the connection
-st.write(spread.url)
-
-sh = client.open(spreadsheetname)
-worksheet_list = sh.worksheets()
