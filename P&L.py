ea558aa6b291552054e09112500a1f9c57717354
h5tusyakdiah <121065596+h5tusyakdiah@users.noreply.github.com>
2023-07-10 10:35:51 +0800

    Update P&L.py

--- P&L.py
+++ P&L.py
@@ -180,6 +180,7 @@ if selected == "Vessel Performance":
 
 # IF DATASET
 elif selected == "Vessel Selection":
+    nama_kapal_input = "KM Julianto Moeliodihardjo"
     st.markdown("<h3 style='text-align: center; color: black;'>Vessel Selection</h3>", unsafe_allow_html=True)
     #st.markdown("<h5 style='text-align: center; color: black;'>Management System</h5>", unsafe_allow_html=True)
     st.text("")
@@ -236,90 +237,7 @@ elif selected == "Vessel Selection":
     hitung = st.button("Run Calculation") 
 
     if hitung:
-        
-        #input data
-        Vessel_List = pd.read_excel('Rekap Vessel Performance.xlsx', sheet_name='Vessel List')
-        OD_Matrix = pd.read_excel('Rekap Vessel Performance.xlsx', sheet_name='OD Matrix')
-        Time_Sheet = pd.read_excel('Rekap Vessel Performance.xlsx', sheet_name='Input - Time Sheet')
-
-        #calculating
-    #ACTUAL
-        PoL_PoD = OD_Matrix[(OD_Matrix.port1 == loading_port) & (OD_Matrix.port2 == discharge_port)]
-        Distance = PoL_PoD.distance.sum()
-
-        Service_speed = 6.7
-        Sailing_Days = Time_Sheet[(Time_Sheet.Vessel_Name == nama_kapal_input) & (Time_Sheet.PoL == loading_port) 
-                                & (Time_Sheet.PoD == discharge_port) & (Time_Sheet.Voyage == voyage)]
-        Sailing_Days = Sailing_Days[(Sailing_Days.Activity == "Sailing to PoD")]
-        Sailing_Days = Sailing_Days.Dur.sum()
-
-        Port_Days= Time_Sheet[(Time_Sheet.Vessel_Name == nama_kapal_input) & (Time_Sheet.PoL == loading_port) 
-                                & (Time_Sheet.PoD == discharge_port) & (Time_Sheet.Voyage == voyage)]
-        Port_Days = Port_Days[(Port_Days.Activity == "Loading") | (Port_Days.Activity == "Discharge")]
-        Port_Days = Port_Days.Dur.sum()
-
-        Waiting_Days = Time_Sheet[(Time_Sheet.Vessel_Name == nama_kapal_input) & (Time_Sheet.PoL == loading_port) 
-                                & (Time_Sheet.PoD == discharge_port) & (Time_Sheet.Voyage == voyage)]
-        Waiting_Days = Waiting_Days[(Waiting_Days.Activity == "Waiting Loading") | (Waiting_Days.Activity == "Waiting Discharge")]
-        Waiting_Days = Waiting_Days.Dur.sum()
-
-        Total_Days = Time_Sheet[(Time_Sheet.Vessel_Name == nama_kapal_input) & (Time_Sheet.PoL == loading_port) 
-                                & (Time_Sheet.PoD == discharge_port) & (Time_Sheet.Voyage == voyage)]
-        Total_Days = Total_Days.Dur.sum()
-        #Sailing_Days, Port_Days, Waiting_Days, Total_Days
-        Bunker_Sailing = (Sailing_Days*LSFO_Price*AE_Fuel_Cons*1000)+(Sailing_Days*LSFO_Price*ME_Fuel_Cons*1000)*-1
-        Bunker_at_Port = (Port_Days*HSD_Price*AE_Fuel_Cons*1000*2*-1)
-        Bunker_Waiting = (Waiting_Days*HSD_Price*AE_Fuel_Cons*1000*-1)
-        #Bunker_Sailing, Bunker_at_Port, Bunker_Waiting
-
-        Total_Fixed_cost = Total_Days*Fixed_Cost*-1
-        Total_cost = Bunker_Sailing + Bunker_at_Port + Bunker_Waiting + Port_Charges + Total_Fixed_cost
-        Revenue = Freight_ton*COB
-        P_and_L = Revenue + Total_cost
-
-        #-------------------------------------------------------------------
-        #IDEAL
-        Distance_2 = Distance 
-        filter_ideal =  Vessel_List[(Vessel_List.Nama_Kapal == nama_kapal_input)]
-        Service_speed_2 = filter_ideal.Speed_Max.sum()
-        Sailing_Days_2 =(Distance_2*2)/(Service_speed_2*24)
-        Port_Days_2 = (COB/Loading_rate)+(COB/Discharge_rate)
-        Waiting_Days_2 = 0
-        Total_Days_2 = Sailing_Days_2+Port_Days_2 + Waiting_Days_2
-        ME_Fuel_Cons_2 = filter_ideal.ME_Cons.sum()
-        AE_Fuel_Cons_2 = filter_ideal.AE_Cons.sum()
-        Bunker_Sailing_2 = ((Sailing_Days_2*LSFO_Price*ME_Fuel_Cons_2*1000)+(Sailing_Days_2*LSFO_Price*AE_Fuel_Cons_2*1000))*-1
-        Bunker_at_Port_2 = (Port_Days_2*HSD_Price*AE_Fuel_Cons_2*1000*2*-1)
-        Bunker_Waiting_2 = (Waiting_Days_2*HSD_Price*AE_Fuel_Cons_2*1000*-1)
-
-        Port_Charges_2 = 8000*COB*-1
-        Fixed_Cost_2 = 815000000/30
-        Total_Fixed_cost_2 = Total_Days_2*Fixed_Cost_2*-1
-        Total_cost_2 = Bunker_Sailing_2 + Bunker_at_Port_2 + Bunker_Waiting_2 + Port_Charges_2 + Total_Fixed_cost_2
-        Revenue_2 = Freight_ton*COB
-        P_and_L_2 = Revenue_2 + Total_cost_2
-    #----------------------------------------------------------------------------
-        Remaining_Time	= Total_Days_2 - Total_Days
-        Time_accuracy =  Total_Days_2/Total_Days*100
-        Speed = Sailing_Days_2/Sailing_Days *100
-        P_and_L_actual	= P_and_L
-        Cost_accuracy  = Total_cost/Total_cost_2 *100
-
-        #Output
-        #st.markdown(f"Kapal **{(nama_kapal_input)}** memiliki nilai")
-        #st.markdown(f"nilai **{(Bunker_Sailing)}**, **{(Bunker_at_Port)}**, **{(Bunker_Waiting)}**")
-
-        #co1A, colB, colC, colD, colE = st.columns([1, 1, 1, 1, 1])
-        #co1A.metric(label = "Remaining Time",
-        #            value = (f"{round(Remaining_Time,2)}"))
-        #colB.metric(label = "Time accuracy",
-        #            value = (f"{round(Time_accuracy,2)} %"))
-        #colC.metric(label = "Speed",
-        #            value = (f"{round(Speed)} %"))
-        #colD.metric(label = "P & L",
-        #            value = (f"Rp. {round(P_and_L_actual)}"))
-        #colE.metric(label = "Cost Accuracy",
-        #            value = (f"{round(Cost_accuracy,2)} %"))
+    
         
         url = "https://batics.pupuk-indonesia.com/"
         st.markdown(f"Lihat hasil disini [link](%s)" % url)
